6cf92ef6d018ee181e947cdc0c87caf9232361ab
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 22:18:20 -0300

    Code Climate

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,7 +4,13 @@ export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <input id="search-input" className="form-control" type="search" placeholder="Search" aria-label="Search" />
+        <input
+          id="search-input"
+          className="form-control"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>

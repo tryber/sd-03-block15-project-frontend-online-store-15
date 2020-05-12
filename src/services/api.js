31bbf5a012ba2c309df1d5d20dcb7420c92be078
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 21:32:22 -0300

    Tirando Async desnecessario

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export function getCategories(countryCode = 'MLB') {
     .then((categoriesList) => categoriesList.json());
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
+export function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const api = `https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`;
   return fetch(`${proxy}${api}`)

c0ee7ff2c6e4bc344e1407276883eb3fdd67b8fd
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-12 12:12:38 -0300

    Listagem de categorias de acordo com requisito 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import * as api from './services/api';
 import ProductList from './pages/ProductList';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
-  api.getCategories().then((categories) => { console.log(categories); });
-  api.getProductsFromCategoryAndQuery('MLB1144', 'playstation').then((playstations) => { console.log(playstations); });
   return (
     <Router>
       <Switch>

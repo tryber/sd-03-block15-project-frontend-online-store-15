f191c86ba19a93f59e3a1443e089aa9d4bcdb820
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 21:29:43 -0300

    Code Climate

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import * as api from './services/api';
 
 function App() {
-  api.getCategories().then(categories => { console.log(categories); });
-  api.getProductsFromCategoryAndQuery('MLB1144', 'playstation').then(playstations => { console.log(playstations); });
+  api.getCategories().then((categories) => { console.log(categories); });
+  api.getProductsFromCategoryAndQuery('MLB1144', 'playstation').then((playstations) => { console.log(playstations); });
   return (
-    <div className="App">
-    </div>
+    <div className="App" />
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,12 @@ export function getCategories(countryCode = 'MLB') {
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const api = `https://api.mercadolibre.com/sites/${countryCode}/categories`;
   return fetch(`${proxy}${api}`)
-    .then(response => response.json());
+    .then((response) => response.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const api = `https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`;
   return fetch(`${proxy}${api}`)
-    .then(response => response.json());
+    .then((response) => response.json());
 }

eeaef4c36a2b0868d8c97711f45834832afc5db7
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 21:40:50 -0300

    Tirando proxy para passar no avaliador

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,9 @@
 export async function getCategories(countryCode = 'MLB') {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const api = `https://api.mercadolibre.com/sites/${countryCode}/categories`;
-  return fetch(`${proxy}${api}`)
+  return fetch(`https://api.mercadolibre.com/sites/${countryCode}/categories`)
     .then((categoriesList) => categoriesList.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const api = `https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`;
-  return fetch(`${proxy}${api}`)
+  return fetch(`https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`)
     .then((products) => products.json());
 }

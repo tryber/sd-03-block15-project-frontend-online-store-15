86cc120538fde832279ec193d3356467a26c7abd
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 21:37:50 -0300

    Colocando async para tirar warning

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
-export function getCategories(countryCode = 'MLB') {
+export async function getCategories(countryCode = 'MLB') {
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const api = `https://api.mercadolibre.com/sites/${countryCode}/categories`;
   return fetch(`${proxy}${api}`)
     .then((categoriesList) => categoriesList.json());
 }
 
-export function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
+export async function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const api = `https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`;
   return fetch(`${proxy}${api}`)

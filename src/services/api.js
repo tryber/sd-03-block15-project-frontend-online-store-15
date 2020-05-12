9179a3d55e90d57dae7ed9ff9201158476166168
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-11 21:13:37 -0300

    Implementando funcoes para chamar APIs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
-export async function getCategories() {
-  // implement here
+export function getCategories(countryCode = 'MLB') {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const api = `https://api.mercadolibre.com/sites/${countryCode}/categories`;
+  return fetch(`${proxy}${api}`)
+    .then(response => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+export async function getProductsFromCategoryAndQuery(categoryId, query, countryCode = 'MLB') {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const api = `https://api.mercadolibre.com/sites/${countryCode}/search?category=${categoryId}&q=${query}`;
+  return fetch(`${proxy}${api}`)
+    .then(response => response.json());
 }

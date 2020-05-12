a2f630301a2d2aa36f28a08d6546a6fb9b445795
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-12 13:50:31 -0300

    Mudancas no CSS e pequena refatoracao

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,6 +3,28 @@ import { Link } from 'react-router-dom';
 import * as api from '../services/api';
 
 export default class ProductList extends Component {
+  static searchBox() {
+    return (
+      <div>
+        <input
+          id="search-input"
+          className="form-control"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          data-testid="query-input"
+        />
+        <button
+          className="btn btn-danger"
+          type="submit"
+          data-testid="query-button"
+        >
+          Search
+        </button>
+      </div>
+    );
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -10,6 +32,7 @@ export default class ProductList extends Component {
       loading: true,
     };
     this.categoriesList = this.categoriesList.bind(this);
+    this.searchBox = this.searchBox.bind(this);
   }
 
   componentDidMount() {
@@ -35,21 +58,19 @@ export default class ProductList extends Component {
     const { loading } = this.state;
     if (loading) return <p>loading</p>;
     return (
-      <div>
-        <input
-          id="search-input"
-          className="form-control"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <Link to="/shoppingCart" data-testid="shopping-cart-button">
-          Carrinho de compras
-        </Link>
-        {this.categoriesList()}
+      <div className="container">
+        <div className="row">
+          <div className="col-3">{this.categoriesList()}</div>
+          <div className="col">
+            {this.searchBox()}
+            <Link to="/shoppingCart" data-testid="shopping-cart-button">
+              Carrinho de compras
+            </Link>
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }

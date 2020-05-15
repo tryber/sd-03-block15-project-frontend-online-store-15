25700103a70a1e0421e7c60ded08a37d76741b0f
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-15 14:05:29 -0300

    Iniciando requisito 9

---
+++ src/components/AddRemoveCartItem.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+
+export class AddRemoveCartItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = ({
+      id: 1,
+      quantity: 1,
+    });
+
+    this.addProductToCart = this.addProductToCart.bind(this);
+    this.subProductToCart = this.subProductToCart.bind(this);
+  }
+
+  addProductToCart() {
+    const { quantity } = this.state;
+    this.setState({ quantity: quantity + 1 });
+  }
+
+  subProductToCart() {
+    const { quantity } = this.state;
+    if (quantity > 0) this.setState({ quantity: quantity - 1 });
+  }
+
+  render() {
+    const { quantity, handleAddCart } = this.state;
+    return (
+      <div>
+        <h5>Quantidade</h5>
+        <button type="button" onClick={this.addProductToCart}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+        <span>{quantity}</span>
+        <button type="button" onClick={this.subProductToCart}>
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+        <input type="button" value="Adicionar ao Carrinho" onClick={() => handleAddCart(quantity)} />
+      </div>
+    );
+  }
+}
+
+export default AddRemoveCartItem;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
+import AddRemoveCartItem from '../components/AddRemoveCartItem';
 
 export class ProductDetail extends Component {
   render() {
-    const { location: { state: { price, thumbnail, title } } } = this.props;
+    const {
+      location: {
+        state: { price, thumbnail, title },
+      },
+    } = this.props;
     return (
-      <div className="card mb-3" style={{ maxWidth: '540px' }}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <img src={thumbnail} className="card-img" alt="..." />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5
-                className="card-title"
-                data-testid="product-detail-name"
-              >
-                {`${title} - R$${price}`}
-              </h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+      <div>
+        <div className="card mb-3" style={{ maxWidth: '540px' }}>
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src={thumbnail} className="card-img" alt="..." />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title" data-testid="product-detail-name">
+                  {`${title} - R$${price}`}
+                </h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <AddRemoveCartItem />
       </div>
     );
   }

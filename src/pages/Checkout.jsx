98bfbe433752256739c7692edfcc217865942d7f
Henrique Rezende <henriquerezendec@gmail.com>
2020-05-18 10:17:56 -0300

    Refatorando

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -4,28 +4,11 @@ import PaymentForms from '../components/PaymentForms';
 import CartTable from '../components/CartTable';
 
 export default class Checkout extends Component {
-  constructor(props) {
-    super(props);
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    this.state = { cartItems };
-    this.updateCartItems = this.updateCartItems.bind(this);
-  }
-
-  updateCartItems() {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    this.setState({ cartItems });
-  }
-
   render() {
-    const { cartItems } = this.state;
     const { updateCartSize } = this.props;
     return (
       <div>
-        <CartTable
-          cartItems={cartItems}
-          updateCartItems={this.updateCartItems}
-          updateCartSize={updateCartSize}
-        />
+        <CartTable updateCartSize={updateCartSize} />
         <BuyerInfo />
         <PaymentForms />
         <button type="submit" className="btn btn-primary">
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,29 +3,12 @@ import { Link } from 'react-router-dom';
 import CartTable from '../components/CartTable';
 
 export class ShoppingCart extends Component {
-  constructor(props) {
-    super(props);
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    this.state = { cartItems };
-    this.updateCartItems = this.updateCartItems.bind(this);
-  }
-
-  updateCartItems() {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    this.setState({ cartItems });
-  }
-
   render() {
-    const { cartItems } = this.state;
     const { updateCartSize } = this.props;
     return (
       <div>
         <Link to="/">Voltar</Link>
-        <CartTable
-          cartItems={cartItems}
-          updateCartItems={this.updateCartItems}
-          updateCartSize={updateCartSize}
-        />
+        <CartTable updateCartSize={updateCartSize} />
         <Link data-testid="checkout-products" to={{ pathname: '/checkout', state: { cartItems } }}>
           Finalizar Compra
         </Link>

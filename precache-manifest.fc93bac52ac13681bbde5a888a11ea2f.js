664117bc596a03a76f13773290f8844aea7d96a6
luizfernando <luizfernandotic@gmail.com>
2020-05-24 21:30:51 -0300

    Updates

---
+++ precache-manifest.fc93bac52ac13681bbde5a888a11ea2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e27c562e7f236ab12d96f2c4304021",
+    "url": "/block15-project-frontend-online-store-15/index.html"
+  },
+  {
+    "revision": "1677d71da0bf838837e7",
+    "url": "/block15-project-frontend-online-store-15/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "d229aaa163cede8b17bc",
+    "url": "/block15-project-frontend-online-store-15/static/css/main.150905e4.chunk.css"
+  },
+  {
+    "revision": "1677d71da0bf838837e7",
+    "url": "/block15-project-frontend-online-store-15/static/js/2.25402ffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/block15-project-frontend-online-store-15/static/js/2.25402ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d229aaa163cede8b17bc",
+    "url": "/block15-project-frontend-online-store-15/static/js/main.a31b6505.chunk.js"
+  },
+  {
+    "revision": "9ca781c83cab3c8380df",
+    "url": "/block15-project-frontend-online-store-15/static/js/runtime-main.abc680a9.js"
+  }
+]);
